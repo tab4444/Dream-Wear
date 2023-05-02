9921abc3a0b770100d976d24a4c31591ffeb1f2f
LautaroE3 <lnespinoza@escuelasproa.edu.ar>
2023-05-02 02:14:48 -0300

    as

--- models/userModel.js
+++ models/userModel.js
@@ -3,16 +3,14 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    admin: { type: Boolean, required: true, default: false },
+    nombreCompleto: {type: String, default: ''},
     wishListQuantity: { type: Number, default: 0},
     wishListContainer:[{type: mongoose.Schema.Types.ObjectId, ref: 'Product'}],
     cartProducts: [{ 
         productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
         quantity: { type: Number, default: 0}
-    }],
-    nombreCompleto: {type: String, default: ''},
-    fechaDeNacimiento: {type: String},
-    genero: {type: String}
+    }]
+    
 });
 
 const User = mongoose.model('User', userSchema);
